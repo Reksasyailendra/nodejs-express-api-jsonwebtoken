b5dfa3b2d62284a73eb4b4b5c008085e131220e7
unknown <Reksasyailendra10@gmail.com>
2022-06-11 10:05:11 +0700

    Taeee

--- src/configs/database.js
+++ src/configs/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
     multipleStatements  : true,
     host                : "sql6.freemysqlhosting.net",
-    user                : "sql6498920",
-    password            : "atwEmgTqwj",
-    database            : "sql6498920"
+    user                : "sql6499013",
+    password            : "YGfJqe6WUY",
+    database            : "sql6499013"
 };
